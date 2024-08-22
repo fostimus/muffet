7e373d9ff53eef7e838b380fa8cb9e67d87e3387
Derek Foster <derekfoster94@gmail.com>
2024-08-21 18:12:35 -0700

    probably need to use the users shell to run this but lets see

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { exec } = require('child_process');
 
 // TODO: options for muffet
 function runMuffet(url) {
+  // TODO: run muffet within node_modules/.bin
+  
   exec(`muffet ${url}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error.message}`);
