eee310cb3148cc0c3ae06ac4e4ebff5ec4d74246
Derek Foster <derekfoster94@gmail.com>
2024-08-21 17:09:22 -0700

    go up a directory and install

--- install-muffet.js
+++ install-muffet.js
@@ -10,21 +10,24 @@ let binaryName;
 
 // if (platform === "linux") {
 //   binaryName =  "muffet-linux"
-// } 
+// }
 if (platform === "darwin") {
-  binaryName = "muffet-macos"
-} 
+  binaryName = "muffet-macos";
+}
 // else if (platform === "win32") {
 //   binaryName =  "muffet-windows.exe"
-// } 
+// }
 else {
   console.error("Unsupported OS!");
   process.exit(1);
 }
 
 const sourcePath = path.join(`${process.cwd()}/binaries`, binaryName);
-const destDir = path.join(process.cwd(), "node_modules/.bin");
-const destPath = path.join(destDir, platform === 'win32' ? 'muffet.exe' : 'muffet');
+const destDir = path.resolve(process.cwd(), "..", "node_modules/.bin");
+const destPath = path.join(
+  destDir,
+  platform === "win32" ? "muffet.exe" : "muffet"
+);
 
 if (!fs.existsSync(destDir)) {
   fs.mkdirSync(destDir, { recursive: true });
@@ -33,4 +36,4 @@ if (!fs.existsSync(destDir)) {
 fs.copyFileSync(sourcePath, destPath);
 fs.chmodSync(destPath, 0o755);
 
-console.log('Muffet binary installed successfully at', destPath);
+console.log("Muffet binary installed successfully at", destPath);
