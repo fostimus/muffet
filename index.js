62416f65ab8d7aebea13eb315e232ea20a950058
Derek Foster <derekfoster94@gmail.com>
2024-08-21 21:13:00 -0700

    started moving to move to user-entered options

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
+const util = require("util");
+const path = require("path");
+const execPromise = util.promisify(exec);
 
-// TODO: options for muffet
-function runMuffet(url) {
-  // TODO: run muffet within node_modules/.bin
-  
-  exec(`muffet ${url}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Stderr: ${stderr}`);
-      return;
-    }
-    console.log(`Muffet Output:\n${stdout}`);
-  });
+const MUFFET_VERSION = "2.10.2";
+
+const muffetPath = path.join(
+  process.cwd(),
+  "node_modules",
+  ".bin",
+  process.platform === "win32" ? "muffet.exe" : "muffet"
+);
+
+async function runMuffet(url, opts) {
+  const {
+    // TODO: support all options
+    maxConnections,
+    bufferSize,
+    color,
+    skipTlsVerification,
+    format,
+    onePageOnly,
+    // TODO: convert headers from array to string
+    headers,
+    timeout,
+    // specific to this wrapper
+    outputMuffetCommand,
+  } = opts || {};
+
+  // TODO: some validation on the options, if user supplied wrong options
+
+  const muffetCommand = `${muffetPath} ${url} ${
+    timeout ? `--timeout=${timeout}s` : ""
+  } ${
+    maxConnections ? `--max-connections=${maxConnections}` : ""
+  } --buffer-size=8192 --color=never --skip-tls-verification --one-page-only --format=json --header="User-Agent: Muffet/${MUFFET_VERSION}"`;
+
+  if (outputMuffetCommand) {
+    console.log("Muffet command:", muffetCommand);
+  }
+
+  try {
+    return await execPromise(muffetCommand);
+  } catch (error) {
+    console.error("Error running muffet", error);
+  }
 }
 
 module.exports = runMuffet;
